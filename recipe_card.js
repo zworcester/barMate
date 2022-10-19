ce26ae97b0b2ff22f866b1eaee245aab8c417022
zworcester <zworcester0@csu.fullerton.edu>
2022-10-19 16:11:24 -0700

    dynamic cocktail view implemented

--- recipe_card.js
+++ recipe_card.js
@@ -17,10 +17,10 @@ class CocktailCard{
 		let ingredients_string = "<tr><th>ABV and Description Here</th></tr>";
 
 		for (const ing in this.ingredients){
-			ingredients_string += "<tr><th>${ing}</th><th>ABV: ${this.ingredients[ing][0]}</th><th>Ratio: ${this.ingredients[ing][1]</th></tr>";
+			ingredients_string += ("<tr><th>" + ing + "</th><th>ABV: " + this.ingredients[ing][0] + "</th><th>Ratio: " + this.ingredients[ing][1] + "</th></tr>");
 		}
 
-		return "<div class=\"col\"><div class=\"card\" id=\"card-${this.name}\"><div class=\"img-box\"><img src=\"${this.image}\"></div><div class=\"content\"><div class=\"details\"><h2>${this.name}<br><span>Ingredients</span></h2><div class=\"data\"><table>${ingredients_string}</table></div><div class =\"action-Button\"><button onmouseleave=\"revertStyle()\" onclick=\"changeStyle()\">Edit</button><script>\nfunction changeStyle(){\n\tvar element = document.getElementById(\"card-${this.name}\");\n\telement.style.paddingBottom = \"500px\";\n}\nfunction revertStyle(){\n\tvar element= document.getElementbyId(\"card-${this.name}\");\n\telement.style.padding=\"200px\";\n}</script><button>Delete</button></div></div></div></div></div>";
+		return "<div class=\"col\"><div class=\"card\" id=\"card-" + this.name + "\"><div class=\"img-box\"><img src=\"" + this.image + "\"></div><div class=\"content\"><div class=\"details\"><h2>" + this.name + "<br><span>Ingredients</span></h2><div class=\"data\"><table>" + ingredients_string + "</table></div><div class =\"action-Button\"><button onmouseleave=\"revertStyle()\" onclick=\"changeStyle()\">Edit</button><script>\nfunction changeStyle(){\n\tvar element = document.getElementById(\"card-" + this.name + "\");\n\telement.style.paddingBottom = \"500px\";\n}\nfunction revertStyle(){\n\tvar element= document.getElementbyId(\"card-" + this.name + "\");\n\telement.style.padding=\"200px\";\n}</script><button>Delete</button></div></div></div></div></div>";
 	}
 
 };
@@ -41,12 +41,10 @@ class CocktailCards{
 		const cardgrid = document.getElementById(this.element_id);
 		cardgrid.replaceChildren();
 
-		const cocktails = this.cocktail_object.cocktails; 
-		for (let x = 0; x < cocktails.length; ++x){
-			let cocktailCard = new CocktailCard(cocktails[x]);
+		for (let x = 0; x < this.cocktail_object.size(); ++x){
+			let cocktailCard = new CocktailCard(this.cocktail_object.getCocktail(x));
 
 			cardgrid.innerHTML += cocktailCard.getCard();
-			delete cocktailCard;
 		}
 	}
 
